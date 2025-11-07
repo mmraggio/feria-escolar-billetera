4b1c9b92df10d68e758fd9b6143697b779f5186a
mmraggio <mmraggio@gmail.com>
2025-11-07 11:47:31 -0300

    Update qrPago.js

--- views/qrPago.js
+++ views/qrPago.js
@@ -16,7 +16,7 @@ function getQRPagoView() {
       <button onclick="switchView('pago')" class="text-indigo-600 hover:text-indigo-800 mr-4 transition duration-200">
         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path></svg>
       </button>
-      <h1 class="text-2xl font-bold text-gray-800">Generar Cobro QR</h1>
+      <h1 class="text-2xl font-bold text-gray-800">QR de Cobro</h1>
     </div>
     <div class="text-center p-4">
       <h3 class="text-xl font-semibold text-green-700">Monto: $${amount.toFixed(2)}</h3>
