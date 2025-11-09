8f61d4c1be592a8abe1dafd38b02ad5bea5418a4
mmraggio <mmraggio@gmail.com>
2025-11-08 23:07:23 -0300

    Update qr.js

--- views/qr.js
+++ views/qr.js
@@ -9,7 +9,7 @@ function getQRView() {
       <button onclick="switchView('home')" class="text-indigo-600 hover:text-indigo-800 mr-4 transition duration-200">
         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path></svg>
       </button>
-      <h1 class="text-2xl font-bold text-gray-800">Mi Código QR (Receptor)</h1>
+      <h1 class="text-2xl font-bold text-gray-800">Mi QR</h1>
     </div>
     <div class="text-center p-4">
       <h3 class="text-lg font-semibold">${userData?.nombre || 'Desconocido'}</h3>
